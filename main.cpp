b8baaf90a698329d495ef8c655218037d003af39
Juhezi <juhezix@163.com>
2020-12-09 18:40:00 +0800

    [Add] 材质

--- main.cpp
+++ main.cpp
@@ -141,10 +141,15 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         lightingShader.use();
-        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("lightColor", 0.5f, 0.5f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos);   // 放在循环外面也行，反正是个常量
         lightingShader.setVec3("viewPos", camera.Position);
+        lightingShader.setVec3("material.ambient",  1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setFloat("material.shininess", 32.0f);
+        lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f,
                                                 100.0f);
--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -1,29 +1,47 @@
 #version 330 core
+
+struct Material {
+    vec3 ambient;// 环境光照
+    vec3 diffuse;// 漫反射光照
+    vec3 specular;// 镜面光照
+    float shininess;//反光度
+};
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec3 Normal;// 当前片段的法向量
 in vec3 FragPos;// 世界空间中的顶点位置
 out vec4 FragColor;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
 uniform vec3 lightPos;// 光源的位置向量
 uniform vec3 viewPos;// 观察者的坐标
 
+uniform Material material;
+uniform Light light;
+
 void main()
 {
-    float ambientStrength = 0.5;// 环境光强度
-    vec3 ambient = ambientStrength * lightColor;
+    // 环境光
+    vec3 ambient =  light.ambient * material.ambient;
 
+    // 漫反射
     vec3 norm = normalize(Normal);// 法线转换为单位向量
     vec3 lightDir = normalize(lightPos - FragPos);// 光源和片段之间的方向向量
     float diff = max(dot(norm, lightDir), 0.0);// 点乘计算当前片段实际的漫反射影响。结果值再乘以光的颜色，得到漫反射分量。
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
-    float specularStrength = 0.5;//    镜面强度
+    // 镜面光
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
